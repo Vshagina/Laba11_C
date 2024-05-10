013035593f368ffbf2bd011407fbb2605535828c
Victoria <123573080+Vshagina@users.noreply.github.com>
2024-05-10 23:52:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    delegate string dayDelegate();
+    static void Main(string[] args)
+    {
+        string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int dayIndex = 0;
+
+        dayDelegate getNextDay = () =>
+        {
+            string currentDay = days[dayIndex];
+
+            dayIndex = (dayIndex + 1) % 7;
+            return currentDay;
+        };
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine(getNextDay());
+        }
+    }
+}
+
